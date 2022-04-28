de1411cf93f93f92373848468b45cb2344c4014b
Yehuda-Edelstein <yeauthor@gmail.com>
2022-04-28 11:38:54 -0400

    dropdows, home button, fade in component scaffold, and 404 component scaffold

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import FadeIn from './components/Main/FadeIn/FadeIn';
 
 function App() {
   return (
     <div> 
       <Header />
+      <Main />
+      
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,34 @@
 import './Header.css'
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(props) {
     return (
         <div>
-            <div>Screenstyle</div>
+            <Link to='/'><div className='title'>Screenstyle | logo maybe</div></Link> 
             <div className='navBar'>
                 <div className='categories' id='dropDown'>Categories
                     <div className='' id='dropDownBox'>
-                        <a href="#">FADE IN?</a>
-                        <a href="#">SLUGS</a>
-                        <a href="#">ACTION</a>
-                        <a href="#">TRANSITIONS</a>
-                        <a href="#">FADE OUT?</a>
+                        <a href="fadein">FADE IN?</a>
+                        <a href="slugs">SLUGS</a>
+                        <a href="action">ACTION</a>
+                        <a href="transitions">TRANSITIONS</a>
+                        <a href="fadeout">FADE OUT?</a>
                     </div>
                 </div>
-                
+                <div className='style' id='dropDown'>Style
+                    <div className='' id='dropDownBox'>
+                        <a href="textdecoration">TEXT DEC'</a>
+                        <a href="montages">MONTAGES</a>
+                        <a href="flashbacks">FLASHBACKS</a>
+                        <a href="musiccues">MUSIC CUES</a>
+                        <a href="voiceovers">(V.O.) , (O.S.)</a>
+                        <a href="beats">BEATS</a>
+                        <a href="intros">INTROS</a>
+                    </div>
+                </div>
+                <div className='contact'>Contact</div>
+                <div className='about'>About</div>
             </div>
         </div>
     );
