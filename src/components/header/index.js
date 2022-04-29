54ef4530a5136dff5f08c28f8ea37bbdbff2571b
Yehuda-Edelstein <yeauthor@gmail.com>
2022-04-29 18:00:16 -0400

    fade in component, needs styling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 function Header(props) {
     return (
         <div>
-            <Link to='/' style={{ color: 'inherit', textDecoration: 'inherit'}}><div className='title'>Screenstyle</div></Link> 
+            <Link to='/' style={{ color: 'inherit', textDecoration: 'inherit'}}><div className='title'>SCREENSTYLE</div></Link> 
             <div className='navBar'>
-                <div className='categories' id='dropDown'>Categories
+                <div className='categories' id='dropDown'>CATEGORIES
                     <div className='' id='dropDownBox'>
                         <a href="fadein">FADE IN?</a>
                         <a href="slugs">SLUGS</a>
@@ -16,7 +16,7 @@ function Header(props) {
                         <a href="fadeout">FADE OUT?</a>
                     </div>
                 </div>
-                <div className='style' id='dropDown'>Style
+                <div className='style' id='dropDown'>STYLE
                     <div className='' id='dropDownBox'>
                         <a href="textdecoration">TEXT DEC'</a>
                         <a href="montages">MONTAGES</a>
@@ -25,10 +25,25 @@ function Header(props) {
                         <a href="voiceovers">(V.O.) , (O.S.)</a>
                         <a href="beats">BEATS</a>
                         <a href="intros">INTROS</a>
+                        {/* add more
+                        1. slang
+                        2. superimpose
+                        3. over black
+                         */}
                     </div>
                 </div>
-                <Link to='contact' style={{ color: 'inherit', textDecoration: 'inherit'}}><div className='contact'>Contact</div></Link>
-                <Link to='about' style={{ color: 'inherit', textDecoration: 'inherit'}}><div className='about'>About</div></Link>
+                <Link to='contact' style={{ color: 'inherit', textDecoration: 'inherit'}}><div className='contact'>CONTACT</div></Link>
+                <Link to='about' style={{ color: 'inherit', textDecoration: 'inherit'}}><div className='about'>ABOUT</div></Link>
+                {/* about section: Screenstyle is a set of aribitrary and completley objective rules for drafting 
+                the most asthetically pleasing screenplay. Screenstyle will not help improve your story -- although
+                I may give advice on tropes I think are overdone, but the idea is along a different vien: 
+                
+                Sometimes content alone isn't good enough. How many scripts were passed on due to poor grammer? 
+                The job of a screenwriter is to deliver the best story, but even if you can do that, if the person
+                reading the thing is lost, why is he (or she -- calm down everybody) going to option it? 
+
+                ...add more
+                 */}
             </div>
         </div>
     );
