42516a301199776810f07f82766b96621ddcdaa9
Yehuda-Edelstein <yeauthor@gmail.com>
2022-05-04 14:11:35 -0400

    FADE IN component updated

--- src/components/main/fadeIn/index.js
+++ src/components/main/fadeIn/index.js
@@ -9,7 +9,9 @@ function FadeIn(props) {
             <div className='blogHeader'>FADE IN?</div>
             <div className='blogText'>
                 The question mark in the dropdown sort of gives it way, but for
-                any Gumps out there let me spell it out for you: <span id='courrier'>FADE IN</span> is unnecessary and totally antiquated. Yeah, it's a staple, so keeping it won't bring down your <a href='about'>screengrade</a> too much, but it's ridiculous. It didn't even make sense when cameras could <span id='italic'>only</span> fade in -- obviously the first line on page 1 is where the script starts, and the last line on <a href='glossary'>110</a> is where it ends. Plus, I think <a href='glossary'>slugs</a> look best as the first words on a page regardless, but more on that in the next section. Either way, I think it's time we let the <span id='courrier'>FADE</span>s become a thing of the past.
+                any Gumps out there let me spell it out for you: <span id='courrier'>FADE IN</span> is unnecessary and totally antiquated. Yeah, it's a staple, so keeping it won't bring down your <a href='about'>screengrade</a>
+                    {/* figure out how to go to about and scroll to What is a Screengrade? section */}
+                too much, but it's ridiculous. It didn't even make sense when cameras could <span id='italic'>only</span> fade in. Obviously the first line on page 1 is where the script starts, and the last line on <a href='glossary/110'>110</a> is where it ends. Plus, I think <a href='glossary/slugs'>slugs</a> look best as the first words on a page regardless, but more on that in the next section. Either way, I think it's time we let the <span id='courrier'>FADE</span>s become a thing of the past.
             <br></br>
                 Here's a comparison:
             <br></br>
