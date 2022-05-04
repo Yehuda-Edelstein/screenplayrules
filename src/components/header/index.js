9fd8c44e773c2e86582d51448832e8a7be3d6ccd
Yehuda-Edelstein <yeauthor@gmail.com>
2022-05-03 22:16:40 -0400

    Header component edits. Needs styling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,16 +5,17 @@ import { Link } from 'react-router-dom';
 function Header(props) {
     return (
         <div>
-            <Link to='/' style={{ color: 'inherit', textDecoration: 'inherit'}}><div className='title'>SCREENPLAY RULES</div></Link>  {/* maybe change to Screengrade */} 
+            {/* maybe have two different paths. Either way at the end have a side by side by side of no rules, rules, then screengrade. Have a screengrade section at the bottom */}
+            <Link to='/' style={{ color: 'inherit', textDecoration: 'inherit'}}><div className='title'>SCREENPLAY RULES</div></Link>
             <div className='navBar'>
                 <div className='categories' id='dropDown'>OUTLINE
                     <div className='' id='dropDownBox'>
                         <a href="fadein">FADE IN?</a>
                         <a href="slugs">SLUGS</a>
                         <a href="action">ACTION</a>
+                        {/* maybe make these two one section */}
                         <a href="intros">INTROS</a>
                         <a href="dialogue">DIALOGUE</a>
-                        {/* <a href="voiceovers">(V.O.) , (O.S.)</a>  */}
                         <a href="transitions">TRANSITIONS</a>
                         <a id='extrasLink' href="textDecoration">EXTRAS</a>
                         <a href="fadeout">FADE OUT?</a>
@@ -23,12 +24,13 @@ function Header(props) {
                 <div className='style' id='dropDown'>EXTRAS
                 {/* need better name for EXTRAS */}
                     <div className='' id='dropDownBox'>
-                        {/* <a href="wrylys">WRYLYS</a> */}
                         {/* <a href="slang">SLANG</a> */}
-                        <a href="textdecoration">TEXT DEC'</a>  
+                        <a href="textdecoration">TEXT DEC'</a> 
+                        {/* <a href="suprise">SUPRISE!'</a>  */}
                         <a href="montages">MONTAGES</a>
                         <a href="flashbacks">FLASHBACKS</a>
                         {/* <a href="superimpose">SUPERIMPOSE</a> */}
+                        {/* <a href="camerashots">CAMERA SHOTS</a> */} {/* ANGLE ON, CLOSE ON, INTERCUTS, ZOOM  */}
                         {/* <a href="reads">READS</a> */}
                         <a href="musiccues">MUSIC CUES</a>
                         {/* <a href="overblack">OVER BLACK</a> */}
@@ -51,9 +53,11 @@ function Header(props) {
 
                     ...add more
                     
-                    ...add a glossery section with terms like screengrade, screengrading, etc. 
+                    ...add a glossery section with terms like screengrade, screengrading, screenblock or block, wrylys, charys or character parethisis, script time, script name vs character name etc. 
 
                     ...add a rulebook (maybe these two add ons be where default component is)
+
+                    ... add a types section with different types of ways to write blocks
 
                     Follow these screenplay rules so that your screenplay rules!
                  */}
@@ -61,6 +65,7 @@ function Header(props) {
                  {/* 
                     default component summary:
 
+                    coming soon -- screengrader
                     start your screengrade tour here - href to first component
                  */}
             </div>
