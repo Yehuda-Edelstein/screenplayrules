2dd92908b05d3d5940ffd08a8ee0782005122faf
Yehuda-Edelstein <yeauthor@gmail.com>
2022-05-09 11:05:13 -0400

    slugs component updated

--- src/components/main/screentour/components/slugs/index.js
+++ src/components/main/screentour/components/slugs/index.js
@@ -33,7 +33,7 @@ function Slugs(props) {
                 </div>
             <br></br>
             <br></br>
-                This abreviation tells us whether the scene will be happening inside or outside, or both. Interior (<span id='courrier'>INT.</span>) for inside, exterior (<span id='courrier'>EXT.</span>) for outside, and a combination (<span id='courrier'>INT.</span>/<span id='courrier'>EXT.</span>) for scenes that switch between the two.
+                This abbreviation tells us whether the scene will be happening inside or outside, or both. Interior (<span id='courrier'>INT.</span>) for inside, exterior (<span id='courrier'>EXT.</span>) for outside, and a combination (<span id='courrier'>INT.</span>/<span id='courrier'>EXT.</span>) for scenes that switch between the two.
             <br></br>
             <br></br>
                 Note: if you're new to screenwriting, which you probably are, refrain from using "<span id='courrier'>INT.</span>/<span id='courrier'>EXT.</span>" Instead, use description to establish a location change, or just cut to a new scene. 
@@ -46,7 +46,7 @@ function Slugs(props) {
                 The middle part of the slugline tells us the actual location of the scene. "<span id='courrier'>EXAMPLE</span>" here could be, "<span id='courrier'>GRANDMA'S HOUSE</span>",  "<span id='courrier'>SIDE OF HIGHWAY</span>", or "<span id='courrier'>INTERNATIONAL SPACE STATION</span>". Pretty much, any explainable location is technically valid. 
             <br></br>
             <br></br>    
-                That being said, it's a bit of an amatuar move to have your locations be overly descriptive. Personally, seeing a slugline that's longer than the description beneath it is alarming. "<span id='courrier'>DUMPSTER BEHIND BURGER JOINT</span>" should just be "<span id='courrier'>BURGER      JOINT</span>". Leave details in the description. 
+                That being said, it's a bit of an amateur move to have your locations be overly descriptive. Personally, seeing a slugline that's longer than the description beneath it is alarming. "<span id='courrier'>DUMPSTER BEHIND BURGER JOINT</span>" should just be "<span id='courrier'>BURGER      JOINT</span>". Leave details in the description. 
                 <div className='sluglineExamplePart'>
                 <div className='sluglineExampleNumber'>3.</div>
                     <div className='sluglineExample'>INT. EXAMPLE <span id='yellow'>- DAY</span> </div> 
@@ -81,7 +81,7 @@ function Slugs(props) {
                 If you want a more specific time than one of these listed, you'll need to superimpose<sup>3</sup> it. As for any questions you may have regarding the reasons for the selected times, check the <Link to='/faqs' className='mainLink'><span id='courrier'>FAQs</span></Link>.
             <br></br>
             <br></br>
-                Now that you understand what a slugline is, and how to write one, the next thing you'll want to do is <span id='bold'>bold</span> them. This will help alleviate the natural, page-to-page monotony screenplays tend to attract. I mean, at the end of the day it's just a bunch of letters, right? Break those scenes up with a bit of <span id='bold'>bold</span>:
+                Now that you understand what a slugline is, and how to write one, the next thing you'll want to do is <span id='bold'>bold</span> them. This will help alleviate the natural, page-to-page monotony screenplays tend to attract. I mean, at the end of the day, it's just a bunch of letters, right? Break those scenes up with a bit of <span id='bold'>bold</span>:
             <br></br>
             <br></br>
                 <div className='pictureContainer'>
@@ -101,7 +101,7 @@ function Slugs(props) {
                         {/* add better animation to drop down */}
                     {handleShow && 
                     <div className='screengradeBlogText'>
-                        As I hinted at earlier, it's a definite plus to land slugs at the tops of you pages, although, it's usually a best case scenario and not always possible. So, don't worry too much about it. That being said, don't ever have a slugline without at least one line of description under it on the <span id='italic'>same</span> page. It's insensitive to tease a reader with a juicy slug and then make them scroll or turn to the next page to see what's going on.
+                        As I hinted at earlier, it's a definite plus to land slugs at the tops of your pages, although, it's usually a best case scenario and not always possible. So, don't worry too much about it. That being said, don't ever have a slugline without at least one line of description below it on the <span id='italic'>same</span> page. It's insensitive to tease a reader with a juicy slug and then make them scroll or turn to the next page to see what's going on.
                         There's a page-turner and then there's this:
                     <br></br>
                         <div className='screengradePictureContainer'>
