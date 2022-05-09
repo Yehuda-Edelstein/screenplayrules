f4cd97c218a112b7311d3fbde235cdb90b09dd2e
Yehuda-Edelstein <yeauthor@gmail.com>
2022-05-09 14:57:22 -0400

    slugs component updated

--- src/components/main/screentour/components/action/index.js
+++ src/components/main/screentour/components/action/index.js
@@ -56,7 +56,7 @@ function Action(props) {
                         <img className='picture' src={longDescriptionWithEdits} alt='long description with extra stuff crossed out' width={'250px'}/>
                     </div>
                 <br></br>
-                    1. For starters, it's too long -- like I said before, trim the fat. Any lines I crossed out were unnecesasary. This isn't a Russian novel, we don't need vivid imagery. Keep it simple. Obviously if you're writing a sci-fi piece set in 3022, some of your scenes will likely need extensive explanations, but if that's not the case, cut to the chase. One-word sentences are fine too. Good, even. They can add a subtle intensity to your description and keep the reader wanting more. Hopefully.
+                    1. For starters, it's too long -- like I said before, trim the fat. Any lines I crossed out were unnecessary. This isn't a Russian novel, we don't need vivid imagery. Keep it simple. Obviously, if you're writing a sci-fi piece set in 3022, some of your scenes will likely need extensive explanations, but if that's not the case, cut to the chase. One-word sentences are fine too. Good, even. They can add a subtle intensity to your description and keep the reader wanting more. Hopefully.
                 <br></br>
                 <br></br>
                     2. The words highlighted in <span id='yellow'>yellow</span> are <span id='courrier'>PROPS</span> and should be in <span id='courrier'>ALL CAPS</span> at first mention. This lets the reader know these items need to be in the script, and are integral to the scene. 
@@ -68,7 +68,7 @@ function Action(props) {
                     3. "<span id='red'><span id='courrier'>Marlboro Red</span></span>" is <span id='red'>red</span> because it's product placement. You're the screenwriter, not the producer. Your job is to say what he's smoking, not what brand. "<span id='courrier'>Marlboro Red</span>" is just "<span id='courrier'>CIGARETTE</span>" -- remember props are in caps the first time mentioned. 
                 <br></br>
                 <br></br>
-                    Note #2: This doesn't apply for scripts whose plots must include certain products (i.e., car movies or era pieces) although writing a spec<sup>1</sup> that hinges on specific merchandies is a risky move better left to those who can make it happen. Because, let's face it, me and you probably can't.
+                    Note: This doesn't apply for scripts whose plots must include certain products (i.e., car movies or era pieces) although writing a spec<sup>1</sup> that hinges on specific merchandise is a risky move better left to those who can make it happen. Because, let's face it, me and you probably can't.
                 <br></br>
                 <br></br>
                     Here we are with a bit more tweaking:
@@ -93,16 +93,16 @@ function Action(props) {
                             extra stuff crossed out' width={'250px'}/>
                             </div>
                             {/* fix the stacked caps for 'a' */}
-                            3. Long paragraphs are bad, just like long sentences are. Better to have three seperate blocks<sup>2</sup> for a total of five lines (seven including white lines<sup>3</sup>) than have one block for a total of five lines.
+                            3. Long paragraphs are bad, just like long sentences are. Better to have three separate blocks<sup>2</sup> for a total of five lines (seven, including white lines<sup>3</sup>) than having one block for a total of five lines.
                     <br></br>
                     <br></br>
-                            4. The "<span id='yellow'><span id='courrier'>S</span></span>" and "<span id='yellow'><span id='courrier'>B</span></span>" are highlighted because parallel capitalized letters<sup>4</sup> (or stacked caps) are offputting. Now, even though only the "<span id='yellow'><span id='courrier'>B</span></span>" is actually problomatic, I highlighted both letters to illuminate on the issue. 
+                            4. The "<span id='yellow'><span id='courrier'>S</span></span>" and "<span id='yellow'><span id='courrier'>B</span></span>" are highlighted because stacked capitalized letters<sup>4</sup> (or stacked caps) are off-putting. Now, even though only the "<span id='yellow'><span id='courrier'>B</span></span>" is actually problematic, I highlighted both letters to illuminate on the issue. 
                     <br></br>
                     <br></br>
-                            5. Similarly, the two "<span id='red'><span id='courrier'>a</span></span>"s are equally (if not more) problomatic because they are the same letter. Whether capitalized or not, stacked letters<sup>5</sup> are never good for your screengrade.
+                            5. Similarly, the two "<span id='red'><span id='courrier'>a</span></span>"s are equally (if not more) problematic because they are the same letter. Whether capitalized or not, stacked letters<sup>5</sup> are never good for your screengrade.
                     <br></br>
                     <br></br>
-                            Note: 4. and 5. apply whether they appear at the start of the line, end of the line, or anywhere in between. You may think it's nuts, but I've done my research and readers do respond better to scripts that minamize stacked caps and stacked letters<sup>*</sup> 
+                            Note: 4. and 5. apply whether they appear at the start of the line, end of the line, or anywhere in between. You may think it's nuts, but I've done my research and readers do respond better to scripts that minimize stacked caps and stacked letters<sup>*</sup> 
                     <br></br>
                     <br></br>
                             Worse than stacking individual letters is doing it with full words, which is just horrifying, and something only AI should be able to get away with. Personally, I wouldn't even start succeeding paragraphs with the same word or letter either. But, hey, that's just me. 
@@ -146,16 +146,16 @@ function Action(props) {
                 You may be wondering:  <span id='italic'>Why do we need his age when it explicitly says he's a high school junior?</span> 
             <br></br>
             <br></br>
-                This is true, we don't really need it. But, there will be times when you have a main character who's out of school, and whose age won't be part of their charatcer description. In those cases you could write <span id='courrier'>(20s)</span>, <span id='courrier'>(30s)</span>, etc. 
+                This is true, we don't really need it. But, there will be times when you have a main character who's out of school, and whose age won't be part of their character description. In those cases you could write <span id='courrier'>(20s)</span>, <span id='courrier'>(30s)</span>, etc. 
             <br></br>
             <br></br>
                 Personally, I always introduce characters with a parenthetical age because it looks cleaner than using commas galore.
             <br></br>
             <br></br>
-                As for the <span id='courrier'>ALL CAPS</span>, remember this only applies to the first time a character is introduced, and only to the script name<sup>2</sup> of said character (notice how "<span id='courrier'>Wilson</span>" is written). 
+                As for the <span id='courrier'>ALL CAPS</span>, remember this only applies to the first time a character is introduced, and only to the script name<sup>6</sup> of said character (notice how "<span id='courrier'>Wilson</span>" is written). 
             <br></br>
             <br></br>
-                This means that when our character is introduced, or has dialouge, he'll be "<span id='courrier'>BRADY</span>" (character names over dialouge are in <span id='courrier'>ALL CAPS</span> too) however, in every other description going forward, he'll just be plain old "<span id='courrier'>Brady</span>". 
+                This means that when our character is introduced, or has dialogue, he'll be "<span id='courrier'>BRADY</span>" (character names over dialogue are in <span id='courrier'>ALL CAPS</span> too) however, in every other description going forward, he'll just be plain old "<span id='courrier'>Brady</span>". 
             <br></br>
             <br></br>
                 Although, this doesn't mean that other characters can't call "<span id='courrier'>Brady</span>" some kind of nickname, just that when we write about him throughout the script, his name must stay consistent for the sake of the reader -- I'll get to a possible exception to this rule in the next section. 
@@ -189,7 +189,7 @@ function Action(props) {
                         The <span id='yellow'>yellow</span> letters here are a less severe version of stacked capitalization, as we saw earlier, where the stacked letters are both in <span id='courrier'>CAPS</span>, but different. 
                     <br></br>
                     <br></br>
-                        Though less severe is still severe so let's just change the line to "<span id='courrier'>In his hand is a CIGARETTE</span>" --
+                        Though less severe is still severe so let's just change the line to "<span id='courrier'>In his hand is a CIGARETTE</span>"
                     <br></br>
                         <div className='screengradePictureContainer'>
                             <img className='picture' src={hh}  alt='with age' width={'250px'}/>
@@ -206,6 +206,27 @@ function Action(props) {
                 <a href="dialogue" id='courrier'>NEXT</a>
                 <a href="slugs" id='courrier'>BACK</a>
            </div>
+           <div className='localGlossary'>
+                <div className='localGlossaryHeader'>LOCAL GLOSS'</div>
+                <ol className='localGlossaryList'>
+                    <li><span id='bold'>Spec</span></li>
+                    <div className='definition'>Short: speculative screenplay. A script written without promise of purchase. </div>
+                    <li><span id='bold'>Blocks</span></li>
+                    <div className='definition'>Parts of a script. Usually alternating in type (i.e., slugs, descriptions, dialogue) each separated by a white line. </div>
+                    <li><span id='bold'>White Lines</span></li>
+                    <div className='definition'>Empty lines of page that go between blocks.</div>
+                    <li><span id='bold'>Stacked Capitalized Letters</span></li>
+                    <div className='definition'>Or stacked caps. Capitalized letters found on top of each other, within the same block or blocks.  Ex: the "C" that starts the second sentence of this definition and the "E" beneath it.</div>
+                    <li><span id='bold'>Stacked Letters</span></li>
+                    <div className='definition'>Identical letters, whether capitalized or not, found on top of each other within the same block or blocks. Ex: italized or not, found on top...  </div>
+                    <li><span id='bold'>Script Name</span></li>
+                    <div className='definition'>The name given to a character for the sake of the script. Used above dialogue and in descriptions. Capitalized the first time mentioned.</div>
+                    <br></br>
+                    <span className='localGlossaryFooter'>* The research I did was to construct three sentences, two with stacked letters in different places, and one without -- all of which contained the exact same words (by adjusting the margins I was able to achieve this). I then asked some friends to read the sentences and choose the one they thought looked the nicest. At first my friends were confused, as the sentences appeared to be identical, but after some slight nudging, every single one of them chose the sentence without stacked letters as being the nicest to look at.</span>
+                    <div className='definition'></div>
+                </ol>
+                <div className='localGlossaryFooter'><span>**full definitions found in glossary above**</span></div>
+            </div>
         </div>
     );
 }
