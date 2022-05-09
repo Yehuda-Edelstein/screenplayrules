f5d8a7a9805f96f22b64dbaea8d6bd8ad7cc53d3
Yehuda-Edelstein <yeauthor@gmail.com>
2022-05-09 00:24:20 -0400

    slug component updated

--- src/components/main/screentour/components/slugs/index.js
+++ src/components/main/screentour/components/slugs/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 import withoutBold from '../../../../../images/slugs/withoutBold.png'
 import withBold from '../../../../../images/slugs/withBold.png'
 import scroll from '../../../../../images/slugs/scroll.png'
@@ -26,27 +27,36 @@ function Slugs(props) {
             <br></br>
             <br></br>
                 A slugline is made up of three parts:
-                <div className='sluglineExample'><span id='yellow'>INT.</span> EXAMPLE - DAY</div> 
+                <div className='sluglineExamplePart'>
+                    <div className='sluglineExampleNumber'>1.</div>
+                    <div className='sluglineExample'><span id='yellow'>INT.</span> EXAMPLE - DAY</div> 
+                </div>
             <br></br>
             <br></br>
                 This abreviation tells us whether the scene will be happening inside or outside, or both. Interior (<span id='courrier'>INT.</span>) for inside, exterior (<span id='courrier'>EXT.</span>) for outside, and a combination (<span id='courrier'>INT.</span>/<span id='courrier'>EXT.</span>) for scenes that switch between the two.
             <br></br>
             <br></br>
                 Note: if you're new to screenwriting, which you probably are, refrain from using "<span id='courrier'>INT.</span>/<span id='courrier'>EXT.</span>" Instead, use description to establish a location change, or just cut to a new scene. 
+                <div className='sluglineExamplePart'>
+                <div className='sluglineExampleNumber'>2.</div>
+                    <div className='sluglineExample'>INT. <span id='yellow'>EXAMPLE</span> - DAY</div> 
+                </div>
             <br></br>
             <br></br>
-                <div className='sluglineExample'>INT. <span id='yellow'>EXAMPLE</span> - DAY</div> 
+                The middle part of the slugline tells us the actual location of the scene. "<span id='courrier'>EXAMPLE</span>" here could be, "<span id='courrier'>GRANDMA'S HOUSE</span>",  "<span id='courrier'>SIDE OF HIGHWAY</span>", or "<span id='courrier'>INTERNATIONAL SPACE STATION</span>". Pretty much, any explainable location is technically valid. 
+            <br></br>
+            <br></br>    
+                That being said, it's a bit of an amatuar move to have your locations be overly descriptive. Personally, seeing a slugline that's longer than the description beneath it is alarming. "<span id='courrier'>DUMPSTER BEHIND BURGER JOINT</span>" should just be "<span id='courrier'>BURGER      JOINT</span>". Leave details in the description. 
+                <div className='sluglineExamplePart'>
+                <div className='sluglineExampleNumber'>3.</div>
+                    <div className='sluglineExample'>INT. EXAMPLE <span id='yellow'>- DAY</span> </div> 
+                </div>
             <br></br>
             <br></br>
-                **CONTINUE HERE**   
+                At last we have the when of your slugline -- the time your scene is happening at. Although this seems simple enough, don't make the mistake of having your times be too nuanced. 
             <br></br>
             <br></br>
-
-
-                Seeing a slugline that's longer than the description beneath it is alarming -- "<span id='courrier'>DUMPSTER BEHIND BURGER JOINT</span>" should just be "<span id='courrier'>BURGER JOINT</span>". Leave details to the description.
-            <br></br> 
-            <br></br> 
-                Also, don't make your <span id='courrier'>TIME</span>s too nuanced. Here's a list of times you should stick to:
+                Here's a list of acceptable times to live by:
                 <div className='timeListContainer'>
                     <ul className='timeList'>
                         <label>Actual<sup>1</sup></label>
@@ -68,37 +78,56 @@ function Slugs(props) {
                         </div>
                     </ul>
                 </div>
-                If you want a more specific time, you'll need to <a href='superimpose'>superimpose</a> it.
-                    {/* add a link back to slugs from superimpose page */}
+                If you want a more specific time than one of these listed, you'll need to superimpose<sup>3</sup> it. As for any questions you may have regarding the reasons for the selected times, check the <Link to='/faqs' className='mainLink'><span id='courrier'>FAQs</span></Link>.
             <br></br>
-                Next you should <span id='bold'>bold</span> your <span id='bold'><span id='courrier'>SLUGLINES</span></span>. 
-                This helps alleviate the natural, page-to-page monotony screenplays tend to attract. I mean, at the end of the day it's just a bunch of letters, right? Break those scenes up with a bit of <span id='bold'>bold</span>:
             <br></br>
-                <img className='picture' src={withoutBold} alt='without bold' width={'250px'}/>
-                <img className='picture' src={withBold} alt='with bold' width={'250px'}/>
+                Now that you understand what a slugline is, and how to write one, the next thing you'll want to do is <span id='bold'>bold</span> them. This will help alleviate the natural, page-to-page monotony screenplays tend to attract. I mean, at the end of the day it's just a bunch of letters, right? Break those scenes up with a bit of <span id='bold'>bold</span>:
             <br></br>
-                Many professionals are already moving away from bare slugs {/* maybe add a href to bare sluglines */} so you don't want to get left behind in the days of old. It's time to accept the days of...
+            <br></br>
+                <div className='pictureContainer'>
+                    <img className='picture' src={withoutBold} alt='without bold' width={'250px'}/>
+                    <img className='picture' src={withBold} alt='with bold' width={'250px'}/>
+                </div>
+            <br></br>
+                Many professionals are already moving away from bare slugs, {/* maybe add a href to bare sluglines */} so don't get left behind in the days of old. It's time to accept the days of...
             </div>
             <br></br>
-            <div className='screengradeHeader' onClick={show}>CLICK HERE TO IMPROVE YOUR SCREENGRADE KNOWLEDGE</div>
-                {/* Figure out better name for screengradeHeader */}
-            {handleShow && 
-            <div className='screengradeBlogText'>
-                <div id='blogTextFirstLine'>
-                    <div className='closeButton' id='courrier' onClick={close}>CLOSE</div>
+                {!handleShow
+                        ? <div className='screengradeHeader' onClick={show}>CLICK HERE TO IMPROVE YOUR SCREENGRADE KNOWLEDGE</div>
+                        : <div className='closeButton' onClick={close}>CLOSE</div>    
+                        } 
+                <div className='screengradeContainer'>
+                        {/* Figure out better name for screengradeHeader */}
+                        {/* add better animation to drop down */}
+                    {handleShow && 
+                    <div className='screengradeBlogText'>
+                        As I hinted at earlier, it's a definite plus to land slugs at the tops of you pages, although, it's usually a best case scenario and not always possible. So, don't worry too much about it. That being said, don't ever have a slugline without at least one line of description under it on the <span id='italic'>same</span> page. It's insensitive to tease a reader with a juicy slug and then make them scroll or turn to the next page to see what's going on.
+                        There's a page-turner and then there's this:
+                    <br></br>
+                        <div className='screengradePictureContainer'>
+                            <img className='picture' src={scroll} alt='with bold' width={'250px'}/>
+                        </div>
+                        Disgusting.
+                    <br></br>
+                    </div> 
+                    }
                 </div>
-                As I hinted at earlier, it's a definite plus to land slugs at the tops of you pages, although, it's usually a best case scenario and not always possible. So, don't worry too much about it. That being said, don't ever have a slugline without at least one line of description under it on the <span id='italic'>same</span> page. It's insensitive to tease a reader with a juicy slug and then make them scroll or turn to the next page to see what's going on.
-                There's a page-turner and then there's this:
-            <br></br>
-                <img className='picture' src={scroll} alt='with bold' width={'250px'}/>
-            <br></br>
-                Disgusting.
-            </div> 
-            }
             <div className='blogFooter'>
                 <a href="action" id='courrier'>NEXT</a>
                 <a href="fadein" id='courrier'>BACK</a>
             </div> 
+            <div className='localGlossary'>
+                <div className='localGlossaryHeader'>LOCAL GLOSS'</div>
+                <ol className='localGlossaryList'>
+                    <li><span id='bold'>Actual</span></li>
+                    <div className='definition'>Universally understood time. Outside of story, not plot-contingent.</div>
+                    <li><span id='bold'>Relative</span></li>
+                    <div className='definition'>Relative </div>
+                    <li><span id='bold'>Superimpose</span></li>
+                    <div className='definition'>Superimpose</div>
+                </ol>
+                <div className='localGlossaryFooter'><span>**full definitions found in glossary above**</span></div>
+            </div>
         </div>
     );
 }
