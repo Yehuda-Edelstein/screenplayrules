95a79449b87a17591f1ac36c6c647761e24451a2
Yehuda-Edelstein <yeauthor@gmail.com>
2022-05-10 13:28:10 -0400

    fade out component updated

--- src/components/main/screentour/components/fadeout/index.js
+++ src/components/main/screentour/components/fadeout/index.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './style.index.css'
+import sceneNoRules from '../../../../../images/fadeOut/sceneNoRules.png'
+import sceneWithRules from '../../../../../images/fadeOut/sceneWithRules.png'
+import sceneWithScreengradeRules from '../../../../../images/fadeOut/sceneWithScreengradeRules.png'
 
 function FadeOut(props) {
+    const [handleShow, setHandleShow] = useState(false)
+
+    function show() {
+        setHandleShow(true)
+    }
+
+    function close() {
+        setHandleShow(false)
+    }
     return (
-        <div>
-            fade out component
+        <div className='container'>
+            <div className='blogHeader'>FADE OUT?</div>
+            <div className='blogText'>
+            Alright. Here's where you deserve your real congratulations, as well as a place to reap the fruits of your labor. 
+        <br></br>
+        <br></br>
+            First off, feel free to continue writing about our story -- any ideas or finished scripts about what happens to <span id='courrier'>Brady</span> and <span id='courrier'>Jules</span>, please send to <a href='mailto:screenplayrules@gmail.com'>screenplayrules@gmail.com</a>. I'd love to see them.
+        <br></br>
+        <br></br>
+            Second, below you will find a side by side comparison of the same scene, one without rules and one with. You decide which one you prefer.
+        <br></br>
+        <br></br>
+            Lastly, for anybody wondering whether or not you should end your script with <span id='courrier'>FADE OUT</span> I advise you to see the question mark and/or refer to the section on <a href='fadein'><span id='courrier'>FADE IN</span></a>.
+        <br></br>
+        <br></br>
+            Good luck and get writing!
+        <br></br>
+        <br></br>
+            <div className='pictureContainer'>
+                <br></br>
+                <div className='pictureLabel'>NO RULES</div>
+                <img className='picture' src={sceneNoRules}  alt='without rules' width={'250px'}  />
+                <br></br>
+                <div className='pictureLabel'>SCREENPLAY RULES</div>
+                <img className='picture' src={sceneWithRules}  alt='with rules' width={'250px'}  />
+            </div>
+        </div>
+        <br></br>
+        <br></br>
+            {!handleShow
+                ? <div className='screengradeHeader' onClick={show}>CLICK HERE FOR SCREENGRADE VERSION</div>
+                : <div className='closeButton' onClick={close}>CLOSE</div>    
+                } 
+            {handleShow &&
+                <div className='screengradeBlogText'>
+                    <div className='screengradePictureContainer'>
+                        <img className='picture' src={sceneWithScreengradeRules}  alt='with screengrade rules' width={'250px'}  />
+                    </div>
+                    Now, that's just darn pretty.
+                </div>
+            }
+        <div className='blogFooter'>
+                <a href="/" id='courrier'>HOME</a>
+                <a href='textdecoration' className='extrasLink'>EXTRAS</a>
+                <a href="dialogue" id='courrier'>BACK</a>
+            </div>
         </div>
     );
 }
