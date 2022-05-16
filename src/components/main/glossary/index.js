0d95544f6cbc88dcdfe4aaf504de57acc6cdbba0
Yehuda-Edelstein <yeauthor@gmail.com>
2022-05-16 13:07:27 -0400

    glossary component route created

---
+++ src/components/main/glossary/index.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+
+
+function Glossary(props) {
+
+    useEffect(() => {
+    }, [])
+
+    return (
+        <div className='container'> 
+            Glossary component
+        </div>
+    );
+}
+
+export default Glossary;
