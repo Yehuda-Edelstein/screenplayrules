4b4aa89315e25f64325d1b665fb6a614acaeb16b
Yehuda-Edelstein <yeauthor@gmail.com>
2022-05-30 17:31:31 -0400

    404 component updated

--- src/components/main/404/index.js
+++ src/components/main/404/index.js
@@ -3,9 +3,11 @@ import "./style.index.scss";
 
 function PageNotFound(props) {
   return (
-    <div className="container">
-      <div className="slugline">INT. 404 - NOT FOUND</div>
-      <div className="description">This route does not exist. Sorry.</div>
+    <div className="notFoundContainer">
+      <div className="notFoundSlugline">INT. 404 - NOT FOUND</div>
+      <div className="notFoundDescription">
+        This route does not exist. Sorry.
+      </div>
     </div>
   );
 }
