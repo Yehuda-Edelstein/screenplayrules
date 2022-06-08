b178f62fb42228c8d80b333827f3ebb1ed698859
Yehuda-Edelstein <yeauthor@gmail.com>
2022-06-08 10:16:52 -0400

    langauge component updated

--- src/components/main/screentour/components/extras/language/index.js
+++ src/components/main/screentour/components/extras/language/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import wolfSample from "../../../../../../images/extras/language/wolfSample.png";
 
 function Language(props) {
   useEffect(() => {
@@ -39,21 +40,30 @@ function Language(props) {
           That being said, it looks bad if you keep using profanity as a crutch.
           A lot of amatuar writers think all intense scenes require a barrage of
           explatives, but they don't. So, use them wisely. Fucker.
-          {/* jordan belfort scene from wolf of wallstreet */}
-          <br></br>
-          <br></br>
+          <div className="pictureContainer">
+            <div className="pictureLabel">
+              <a
+                href="https://www.scriptslug.com/script/the-wolf-of-wall-street-2013"
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                The Wolf of Wall Street (2013)
+              </a>
+            </div>
+            <img
+              className="picture"
+              src={wolfSample}
+              alt="The Wolf of Wall Street (2013)"
+            />
+          </div>
           Anyways, this section is actually talking about foriegn languages and
           subtitles. It's pretty simple, conceptially. You have a character
           that's from a foriegn country or different galaxy, and you want them
           to speak in a specific language.
           <br></br>
           <br></br>
-          This is totally fine, however there are common mistakes made regarding
-          the subject. Here are some rules to go by:
-          <br></br>
-          <br></br>
-          1. Don't spell out the foriegn language. Instead write the sentence
-          how you normally would and include a wryly:
+          This is totally fine, just don't spell out the foriegn language.
+          Instead write the sentence how you normally would and include a wryly:
           <br></br>
           <br></br>
           Note: if your character is speaking the language badly for comedic
