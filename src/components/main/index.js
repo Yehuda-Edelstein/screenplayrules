c4b352d94f01a13106503f5c0bac0f301fa75ad7
Yehuda-Edelstein <yeauthor@gmail.com>
2022-06-08 10:29:11 -0400

    screentour intro page and main style/index updated for seo

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -33,47 +33,45 @@ import PageNotFound from "./404";
 
 function Main(props) {
   return (
-    <div>
-      <div className="main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* USER ROUTES */}
-          {/* <Route path='login' element={<LogIn />} />
+    <div className="main">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* USER ROUTES */}
+        {/* <Route path='login' element={<LogIn />} />
                 <Route path='signup' element={<SignUp />} /> */}
-          {/* NAV ROUTES */}
-          {/* <Route path="rulebook" element={<Rulebook />} /> */}
-          <Route path="glossary" element={<Glossary />} />
-          <Route path="contact" element={<Contact />} />
-          {/* BLOG ROUTES */}
-          {/* <Route path='blog' element={<BlogList />} /> 
+        {/* NAV ROUTES */}
+        {/* <Route path="rulebook" element={<Rulebook />} /> */}
+        <Route path="glossary" element={<Glossary />} />
+        <Route path="contact" element={<Contact />} />
+        {/* BLOG ROUTES */}
+        {/* <Route path='blog' element={<BlogList />} /> 
                 <Route path='blog/:id' element={<BlogDetail />} />
                 <Route path='blog/:id/edit' element={<EditBlog />} />
                 <Route path='blog/new' element={<NewBlog />} />     */}
-          <Route path="screentour" element={<Screentour />} />
-          {/* SCREENTOUR ROUTES */}
-          <Route path="screentour/fadein" element={<FadeIn />} />
-          <Route path="screentour/slugs" element={<Slugs />} />
-          {/* add an establishing bonus here, or in extras section */}
-          <Route path="screentour/action" element={<Action />} />
-          <Route path="screentour/dialogue" element={<Dialogue />} />
-          <Route path="screentour/transitions" element={<Transitions />} />
-          <Route path="screentour/fadeout" element={<FadeOut />} />
-          {/* SCREENTOUR EXTRA ROUTES */}
-          <Route path="screentour/extras" element={<Extras />} />
-          <Route path="screentour/textdecoration" element={<TextDec />} />
-          <Route path="screentour/slang" element={<Slang />} />
-          <Route path="screentour/camerashots" element={<CameraShots />} />
-          <Route path="screentour/language" element={<Language />} />
-          {/* reading */}
-          <Route path="screentour/montages" element={<Montages />} />
-          <Route path="screentour/flashbacks" element={<Flashbacks />} />
-          <Route path="screentour/musiccues" element={<MusicCues />} />
-          <Route path="screentour/beats" element={<Beats />} />
-          <Route path="screentour/superimpose" element={<Superimpose />} />
-          <Route path="screentour/overblack" element={<OverBlack />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </div>
+        <Route path="screentour" element={<Screentour />} />
+        {/* SCREENTOUR ROUTES */}
+        <Route path="screentour/fadein" element={<FadeIn />} />
+        <Route path="screentour/slugs" element={<Slugs />} />
+        {/* add an establishing bonus here, or in extras section */}
+        <Route path="screentour/action" element={<Action />} />
+        <Route path="screentour/dialogue" element={<Dialogue />} />
+        <Route path="screentour/transitions" element={<Transitions />} />
+        <Route path="screentour/fadeout" element={<FadeOut />} />
+        {/* SCREENTOUR EXTRA ROUTES */}
+        <Route path="screentour/extras" element={<Extras />} />
+        <Route path="screentour/textdecoration" element={<TextDec />} />
+        <Route path="screentour/slang" element={<Slang />} />
+        <Route path="screentour/camerashots" element={<CameraShots />} />
+        <Route path="screentour/language" element={<Language />} />
+        {/* reading */}
+        <Route path="screentour/montages" element={<Montages />} />
+        <Route path="screentour/flashbacks" element={<Flashbacks />} />
+        <Route path="screentour/musiccues" element={<MusicCues />} />
+        <Route path="screentour/beats" element={<Beats />} />
+        <Route path="screentour/superimpose" element={<Superimpose />} />
+        <Route path="screentour/overblack" element={<OverBlack />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
