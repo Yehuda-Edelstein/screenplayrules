6abdb115bd0b41eb40aa3bd7322e543f9946d586
Yehuda-Edelstein <yeauthor@gmail.com>
2022-06-17 17:20:29 -0400

    fade in updated

--- src/components/main/screentour/components/fadeIn/index.js
+++ src/components/main/screentour/components/fadeIn/index.js
@@ -36,35 +36,64 @@ function FadeIn(props) {
         <div className="blog">
           <div className="blogText">
             When used, the words "<span id="courrier">FADE IN</span>" are the
-            first words found in a screenplay. Their point is to introduce the
-            shift from black screen (before the movie starts) to the first scene
-            we, the audience, will see. Simply put, every line of script that
-            goes beneath them will be acted, spoken, filmed, and produced, up
-            until the last words "<span id="courrier">FADE OUT</span>" which
-            we'll get to later.
+            first words found in a screenplay, written as such:
             <br></br>
             <br></br>
+            <div className="divider">
+              <span id="dividerCourrier">FADE IN:</span>
+            </div>
+            <br></br>
+            Their point is to introduce the shift from black screen (before the
+            movie starts) to the first scene we, the audience, get to see.
+            <div className="gifContainer">
+              <img
+                className="gif"
+                src="https://media.giphy.com/media/rHlfYmdLr6SK7thB9n/giphy.gif"
+                alt="The Wolf of Wall Street opening scene"
+              />
+            </div>
+            {/* <div className="gifContainer">
+              <img
+                className="gif"
+                src="https://media.giphy.com/media/IYUY3PPXeNYo8/giphy.gif"
+                alt="A Trip To The Moon (1904)"
+                height="350px"
+              />
+            </div> */}
+            Simply put, every line of script that goes beneath them will be
+            acted, spoken, filmed, and produced, up until the last words "
+            <span id="courrier">FADE OUT</span>" which we will get to later.
+            <br></br>
+            <br></br>
+            <div id="border"></div>
             Now, how do I personally feel about starting my scripts this way?
+            <br></br>
+            <br></br>
             The question mark in the title sort of gives it way, but for any
             Gumps out there, let me spell it out for you:
             <br></br>
-            <br></br>"<span id="courrier">FADE IN</span>" is unnecessary and
-            totally antiquated.
             <br></br>
+            <div className="divider">
+              It's unnecessary and totally antiquated.
+            </div>
             <br></br>
             It's a staple of the craft, so keeping it won't bring down your
-            screengrade<sup>1</sup> too much, but it's ridiculous. It didn't
-            even make sense when cameras could <span id="italic">only</span>{" "}
-            fade in -- obviously the first line on page 1 is where the script
-            starts, and the last line on 110<sup>2</sup> is where it ends.
+            screengrade<sup>1</sup> too much, but it's ridiculous.
+            <br></br>
+            <br></br>
+            It didn't even make sense when cameras could{" "}
+            <span id="italic">only</span> fade in. Obviously the first line on
+            page 1 is where the script starts, and the last line on 110
+            <sup>2</sup> is where it ends.
             <br></br>
             <br></br>
             Plus, I think slugs<sup>3</sup> look best as the first words on a
-            page regardless, but more on that in the next section. Either way, I
-            think it's time we let the fades become a thing of the past.
+            page regardless, but more on that in the next section.
             <br></br>
             <br></br>
-            Here's a comparison:
+            <div id="border"></div>
+            Either way, I think it's time we let the fades become a thing of the
+            past. Here's a comparison:
             <div className="pictureContainer">
               <img className="picture" src={withFadeIn} alt="with fade in" />
               <div className="pictureLabel">
@@ -79,6 +108,9 @@ function FadeIn(props) {
             It doesn't seem like much, but I think it helps give the script a
             cleaner start. Trim the fat, you know?
             <br></br>
+            <br></br>
+            Note: the second line will be explained in the next section, don't
+            worry.
           </div>
         </div>
         <div className="localGlossary">
@@ -90,7 +122,7 @@ function FadeIn(props) {
             <div className="definition">
               A non plot-related and completely arbitrary metric for grading a
               screenplay, based purely on its visual aesthetic. See: about
-              section for further explanation.
+              section in footer for further explanation.
             </div>
             <li>
               <span id="bold">110</span>
