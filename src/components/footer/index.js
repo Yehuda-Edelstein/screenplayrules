7996b1bebac6fb704d2c0d187c9bed070af68d91
Yehuda-Edelstein <yeauthor@gmail.com>
2022-06-21 10:37:27 -0400

    footer component updated

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.index.scss";
 
 function Footer(props) {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="copyright">©️2022 Screenplay Rules</div>
+      <Link to="about" style={{ color: "inherit", textDecoration: "inherit" }}>
+        <div className="footerLink">ABOUT</div>
+      </Link>
+      <Link
+        to="contact"
+        style={{ color: "inherit", textDecoration: "inherit" }}
+      >
+        <div className="footerLink">CONTACT</div>
+      </Link>
+    </div>
+  );
 }
 
 export default Footer;
